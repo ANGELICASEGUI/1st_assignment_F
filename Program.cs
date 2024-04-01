14054f31911dbc29e3863c1dbbe9007f4ea071a3
ANGELICASEGUI <165160266+ANGELICASEGUI@users.noreply.github.com>
2024-04-01 18:13:43 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Net.Security;
+namespace _1st_assignment_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                string username = "ADMIN";
+                string password = "ADMIN1234";
+                int attempts = 3;
+
+                while (attempts > 0)
+                {
+                    Console.Write("Username: ");
+                    string a = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string b = Console.ReadLine();
+
+                    if (a == username && b == password)
+                    {
+                        attempts--;
+                        Console.WriteLine("Login successful"); break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect identification");
+                        attempts --;
+                    }   
+
+                    }
+                    if (attempts == 0) 
+                    {
+                        Console.WriteLine( "failed logging in! try again later");
+                        
+                        
+                    }
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {
+
+
+
+
+                }
+
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
+
+        
